b533b0909fce90437e8e2931caf2285219e8a591
Dean Camera <dean@fourwalledcubicle.com>
2015-06-24 22:08:06 +1000

    Add load averages.

--- status.py
+++ status.py
@@ -2,6 +2,7 @@
 
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 import subprocess
+import os
 
 class RequestHandler(BaseHTTPRequestHandler):
     def _get_network_status(self):
@@ -17,12 +18,12 @@ class RequestHandler(BaseHTTPRequestHandler):
 
     def _write_service_info(self, f, service):
         f.write("<b>{}</b> <br/>".format(service))
-	f.write("<code>")
-	for i in self._get_service_status(service):
-	  f.write(i)
+        f.write("<code>")
+        for i in self._get_service_status(service):
+          f.write(i)
           if i == '\n':
             f.write("<br/>")
-	f.write("</code> <br/>")
+        f.write("</code> <br/>")
         f.write("""<a href="/{}/start">START</a> | <a href="/{}/stop">STOP</a>""".format(service, service))
         f.write("""<br/><br/>""")
    
@@ -39,30 +40,30 @@ class RequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self._writeheaders()
-	self.wfile.write("""<html><head><title>CRISPIN - STATUS</title>""")
-	delay = 0 if self.path != "/" else 10
+        self.wfile.write("""<html><head><title>CRISPIN - STATUS</title>""")
+        delay = 0 if self.path != "/" else 10
         self.wfile.write(""" <META http-equiv="refresh" content="{};URL=/"> """.format(delay))
-	self.wfile.write("""
-		<style>
-			code {
-		 	 width: 100em;
-		 	 display: inline-block;
-		  	padding: 10px;
-		  	margin: 5px;
-			  background: #ECECEC;
-			  border-radius: 20px;
-			}
+        self.wfile.write("""
+                <style>
+                        code {
+                          width: 100em;
+                          display: inline-block;
+                          padding: 10px;
+                          margin: 5px;
+                          background: #ECECEC;
+                          border-radius: 20px;
+                        }
 
-			h1 {
-			  font-family: serif;
-			}
+                        h1 {
+                          font-family: serif;
+                        }
 
-			a {
-			  font-weight: bold;
-			}
-		</style>
-		""")
-	self.wfile.write("""</head><body>""")
+                        a {
+                          font-weight: bold;
+                        }
+                </style>
+                """)
+        self.wfile.write("""</head><body>""")
 
         # NETWORK STATUS
         self.wfile.write("""<h1>Network Status:</h1>""")
@@ -71,19 +72,23 @@ class RequestHandler(BaseHTTPRequestHandler):
              self.wfile.write(l + """<br/>""")
         self.wfile.write("""</code>""")
 
-
         # SERVICE STATUS/CONTROL
         services = ["transmission-daemon", "openvpn"]
         self.wfile.write("""<h1>Service Status:</h1>""")
         for s in services:
             self._write_service_info(self.wfile, s)
-	
-	    if self.path == "/{}/start".format(s):
-		self._service_control(s, "start")
-	    elif self.path == "/{}/stop".format(s):
-	        self._service_control(s, "stop")
-
+        
+            if self.path == "/{}/start".format(s):
+                self._service_control(s, "start")
+            elif self.path == "/{}/stop".format(s):
+                self._service_control(s, "stop")
         self.wfile.write("""</body></html>""")
+
+        # LOAD AVERAGE
+        load_average = os.getloadavg()
+        self.wfile.write("""<h1>Load Status</h1>""")
+        self.wfile.write("<code><b>5 min:</b> %.02f, <b>10 min:</b> %.02f, <b>15 min:</b> %.02f</code>" % (load_average[0], load_average[1], load_average[2]))
+        self.wfile.write("""</code>""")
 
 serveraddr = ('', 8080)
 srvr = HTTPServer(serveraddr, RequestHandler)
