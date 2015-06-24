0637e948c295fbb4641849168a5acca4d45b5d62
Dean Camera <dean@fourwalledcubicle.com>
2015-06-24 22:32:00 +1000

    Refactor script to add tunables, use basic routing layer.

--- status.py
+++ status.py
@@ -3,47 +3,50 @@
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 import subprocess
 import os
+import socket
 
-class RequestHandler(BaseHTTPRequestHandler):
+
+PORT     = 8080
+HOSTNAME = socket.gethostname()
+
+
+class ServiceStatusPage():
+    services = ["transmission-daemon", "openvpn"]
+
     def _get_network_status(self):
         return subprocess.check_output(["ifconfig"])
 
     def _get_service_status(self, service):
-        try:        
+        try:
              stat = subprocess.check_output(["sudo", "service", service, "status"], stderr=subprocess.STDOUT)
         except:
              stat = "Not Running"
 
         return stat
 
-    def _write_service_info(self, f, service):
-        f.write("<b>{}</b> <br/>".format(service))
-        f.write("<code>")
+    def _write_service_info(self, output, service):
+        output.write("<b>{}</b> <br/>".format(service))
+        output.write("<code>")
         for i in self._get_service_status(service):
-          f.write(i)
+          output.write(i)
           if i == '\n':
-            f.write("<br/>")
-        f.write("</code> <br/>")
-        f.write("""<a href="/{}/start">START</a> | <a href="/{}/stop">STOP</a>""".format(service, service))
-        f.write("""<br/><br/>""")
-   
+            output.write("<br/>")
+        output.write("</code> <br/>")
+        output.write("""<a href="/%s/start">START</a> | <a href="/%s/stop">STOP</a>""" % (service, service))
+        output.write("""<br/><br/>""")
+
     def _service_control(self, service, command):
         subprocess.call(["sudo", "service", service, command])
 
-    def _writeheaders(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+    def accept(self, path):
+        return True
 
-    def do_HEAD(self):
-        self._writeheaders()
+    def route(self, path, output):
+        refresh_delay = 0 if path != "/" else 10
 
-    def do_GET(self):
-        self._writeheaders()
-        self.wfile.write("""<html><head><title>CRISPIN - STATUS</title>""")
-        delay = 0 if self.path != "/" else 10
-        self.wfile.write(""" <META http-equiv="refresh" content="{};URL=/"> """.format(delay))
-        self.wfile.write("""
+        output.write("""<html><head><title>%s - STATUS</title>""" % (HOSTNAME))
+        output.write(""" <META http-equiv="refresh" content="%s;URL=/"> """ % (refresh_delay))
+        output.write("""
                 <style>
                         code {
                           width: 100em;
@@ -63,34 +66,61 @@ class RequestHandler(BaseHTTPRequestHandler):
                         }
                 </style>
                 """)
-        self.wfile.write("""</head><body>""")
+        output.write("""</head><body>""")
 
         # NETWORK STATUS
-        self.wfile.write("""<h1>Network Status:</h1>""")
-        self.wfile.write("""<code>""")
+        output.write("""<h1>Network Status:</h1>""")
+        output.write("""<code>""")
         for l in self._get_network_status().split('\n'):
-             self.wfile.write(l + """<br/>""")
-        self.wfile.write("""</code>""")
+             output.write(l + """<br/>""")
+        output.write("""</code>""")
 
         # SERVICE STATUS/CONTROL
-        services = ["transmission-daemon", "openvpn"]
-        self.wfile.write("""<h1>Service Status:</h1>""")
-        for s in services:
-            self._write_service_info(self.wfile, s)
-        
-            if self.path == "/{}/start".format(s):
+        output.write("""<h1>Service Status:</h1>""")
+        for s in self.services:
+            self._write_service_info(output, s)
+
+            if path == "/%s/start" % (s):
                 self._service_control(s, "start")
-            elif self.path == "/{}/stop".format(s):
+            elif path == "/%s/stop" % (s):
                 self._service_control(s, "stop")
-        self.wfile.write("""</body></html>""")
+        output.write("""</body></html>""")
 
         # LOAD AVERAGE
         load_average = os.getloadavg()
-        self.wfile.write("""<h1>Load Status</h1>""")
-        self.wfile.write("<code><b>5 min:</b> %.02f, <b>10 min:</b> %.02f, <b>15 min:</b> %.02f</code>" % (load_average[0], load_average[1], load_average[2]))
-        self.wfile.write("""</code>""")
+        output.write("""<h1>Load Status</h1>""")
+        output.write("<code><b>5 min:</b> %.02f, <b>10 min:</b> %.02f, <b>15 min:</b> %.02f</code>" % (load_average[0], load_average[1], load_average[2]))
+        output.write("""</code>""")
 
-serveraddr = ('', 8080)
+
+class RequestHandler(BaseHTTPRequestHandler):
+    routers = [ ServiceStatusPage() ]
+
+    def _find_routing(self, path):
+        for router in self.routers:
+            if router.accept(path):
+                return router
+
+        return None
+
+    def _writeheaders(self, path):
+        routing = self._find_routing(path)
+
+        self.send_response(200 if routing is not None else 404)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_HEAD(self):
+        self._writeheaders(self.path)
+
+    def do_GET(self):
+        routing = self._find_routing(self.path)
+
+        self._writeheaders(self.path)
+        routing.route(self.path, self.wfile)
+
+
+serveraddr = ('', PORT)
 srvr = HTTPServer(serveraddr, RequestHandler)
 srvr.serve_forever()
-
+s
